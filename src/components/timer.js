b78bd9b8f97eea7845cb55b231617c5548f06d4b
emipeanz <epea390@aucklanduni.ac.nz>
2019-06-10 09:57:07 +1200

    Remove unused methods

--- src/components/timer.js
+++ src/components/timer.js
@@ -49,35 +49,7 @@ AFRAME.registerComponent('timer', {
         }
         console.log("total time:", this.getTotalTimeElapsed())
         console.log("active time: ", this.totalActiveTime)
-        console.log(typeof(this.totalActiveTime))
         console.log("idle time: ", this.totalIdleTime)
         console.log("")
-    },
-
-
-//set the digits to the updated time left.
-
-//Define the events after the time is over.
-    TimeUp: function(){
-        alert("TIME UP");
-    },
-    update: function (oldData) {
-        var data = this.data;
-        var el = this.el;
-
-        // If `oldData` is empty, then this means we're in the initialization process.
-        // No need to update.
-        if (Object.keys(oldData).length === 0) { return; }
-
-        // Material-related properties changed. Update the material.
-        if (data.DigitsColor !== oldData.DigitsColor) {
-
-            //we are slicing color as the returned color is of the form "#000000" and we require only the digits.
-            var col= "0x"+data.DigitsColor.toString(16).slice(-6);
-            for(var i=0;i<14;i++) //updating the color of all the children of entity
-            {
-                el.object3D.children[0].children[i].children[0].material.color.setHex(col);
-            }
-        }
     }
 })
