597b1610ca3255e0c21661a012069fb6ae990cac
mbea966 <beagleymaddie@gmail.com>
2019-06-10 12:56:07 +1200

    Wedge appears when trackpad pressed

--- src/components/wedge-generator.js
+++ src/components/wedge-generator.js
@@ -11,6 +11,12 @@ AFRAME.registerComponent('wedge-generator', {
 
         el.sceneEl.addEventListener('generateWedge', function(){
             console.log("handling wedge generation event")
+            var wedge = document.createElement('a-cone')
+            wedge.setAttribute("scale", "0.04 1 0.05")
+            wedge.setAttribute("position", "0 1.5 0")
+            wedge.setAttribute("height", "0.1")
+
+            self.el.sceneEl.appendChild(wedge)
         })
 
     },
