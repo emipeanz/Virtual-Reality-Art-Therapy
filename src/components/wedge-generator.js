3b2214c9ecb3b250de69d20ef3f5da03243d6c76
mbea966 <beagleymaddie@gmail.com>
2019-06-13 12:40:42 +1200

    Set the origin of the bounded box on first middle button click.

--- src/components/wedge-generator.js
+++ src/components/wedge-generator.js
@@ -15,9 +15,8 @@ AFRAME.registerComponent('wedge-generator', {
         var maxForwardReach = 0.5; // Reaching forwards
 
         //Resting position of controller, used as the origin of the bounding box
-        var startXPos = 0;
-        var startYPos = 0;
-        var startZPos = 0;
+        this.originControllerPosition = new THREE.Vector3();
+        this.originSet = false;
 
         this.controllerPosition = new THREE.Vector3();
 
@@ -26,60 +25,71 @@ AFRAME.registerComponent('wedge-generator', {
         this.camera = document.querySelector('#acamera');
 
         el.sceneEl.addEventListener('position-changed', function (evt) {
-            if (evt.detail.position.x !== 0 && evt.detail.position.y !== 0 && evt.detail.position.z !== 0) {
+            if ((evt.detail.position.x !== 0 && evt.detail.position.y !== 0 && evt.detail.position.z !== 0) &&
+                (evt.detail.position.x !== 1 && evt.detail.position.y !== 1 && evt.detail.position.z !== 1) &&
+                (evt.detail.position.x !== undefined && evt.detail.position.y !== undefined && evt.detail.position.z !== undefined)) {
                 self.controllerPosition = evt.detail.position;
             }
         })
 
         el.sceneEl.addEventListener('generateWedge', function(){
             // remove old wedge to make room for new one
-            var oldWedge = document.querySelector('a-cone');
-            if(oldWedge !== null){
-                oldWedge.parentElement.removeChild(oldWedge);
-            }
-
-            var wedge = document.createElement('a-cone')
-
-            global.navigator = global.window.navigator;
-            self.gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
-            self.VRDisplays = navigator.getVRDisplays() ?  navigator.getVRDisplays() : [];
-
-            // Depending on the boolean variable, let the users place the wedge where they want, or randomly
-            if(self.userControlledWedgeLocation){
-                // Position wedge relative to position of controller
-                var position = self.controllerPosition;
+            if (!self.originSet) {
+                console.log("SETTING ORIGIN TO ", self.controllerPosition);
+                self.originControllerPosition = self.controllerPosition;
+                self.originSet = true;
             }
             else{
-                //Generate the x, y and z coordinates randomly in the centre of the screen
-                var x = (Math.random())* (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                // elevate so not on ground
-                var y = Math.random() + 0.5
-                var z = (Math.random())* (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                var position = x + " " + y + " " + z;
+                var oldWedge = document.querySelector('a-cone');
+                if(oldWedge !== null){
+                    oldWedge.parentElement.removeChild(oldWedge);
+                }
+
+                var wedge = document.createElement('a-cone')
+
+                global.navigator = global.window.navigator;
+                self.gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
+                self.VRDisplays = navigator.getVRDisplays() ?  navigator.getVRDisplays() : [];
+
+                // Depending on the boolean variable, let the users place the wedge where they want, or randomly
+                if(self.userControlledWedgeLocation){
+                    // Position wedge relative to position of controller
+                    var position = self.controllerPosition;
+                }
+                else{
+                    //Generate the x, y and z coordinates randomly in the centre of the screen
+                    var x = (Math.random())* (Math.floor(Math.random()*2) === 1 ? 1 : -1);
+                    // elevate so not on ground
+                    var y = Math.random() + 0.5
+                    var z = (Math.random())* (Math.floor(Math.random()*2) === 1 ? 1 : -1);
+                    var position = x + " " + y + " " + z;
+                }
+
+                // Height is between 0.1 and 0.5
+                height = Math.random() * 0.4 + 0.1;
+
+                //Flowers should have between 4 and 8 petals
+                var numPetals = Math.round(Math.random() * 4) + 4;
+                //Radius is calculated based on number of petals
+                var radiusBottom = height *  Math.tan(Math.PI / numPetals);
+
+                var rotation = self.camera.getAttribute('rotation');
+                rotation.x = 0;
+                rotation.z = 0;
+
+                wedge.setAttribute("scale", "0.1 1 1")
+                wedge.setAttribute("position", position)
+                wedge.setAttribute("height", height)
+                wedge.setAttribute("color", "#ffffff")
+                wedge.setAttribute("opacity", "0.2")
+                wedge.setAttribute("geometry" , "radiusBottom:" + radiusBottom);
+                wedge.setAttribute('material',  "wireframe:true");
+                wedge.setAttribute('rotation', rotation);
+
+                self.el.sceneEl.appendChild(wedge)
             }
 
-            // Height is between 0.1 and 0.5
-            height = Math.random() * 0.4 + 0.1;
 
-            //Flowers should have between 4 and 8 petals
-            var numPetals = Math.round(Math.random() * 4) + 4;
-            //Radius is calculated based on number of petals
-            var radiusBottom = height *  Math.tan(Math.PI / numPetals);
-
-            var rotation = self.camera.getAttribute('rotation');
-            rotation.x = 0;
-            rotation.z = 0;
-
-            wedge.setAttribute("scale", "0.1 1 1")
-            wedge.setAttribute("position", position)
-            wedge.setAttribute("height", height)
-            wedge.setAttribute("color", "#ffffff")
-            wedge.setAttribute("opacity", "0.2")
-            wedge.setAttribute("geometry" , "radiusBottom:" + radiusBottom);
-            wedge.setAttribute('material',  "wireframe:true");
-            wedge.setAttribute('rotation', rotation);
-
-            self.el.sceneEl.appendChild(wedge)
     })
     },
     tick: function() {
